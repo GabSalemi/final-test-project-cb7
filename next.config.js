4284b3a2717bf7284e9ca7baf64a0a3c8a2b1e82
GabSalemi <salemi.gabriele94@gmail.com>
2023-08-03 15:51:44 +0200

    chore: install sass, dependencies, folder and files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: `
+  @import "./src/styles/sassUtils/variables.scss";
+  @import "./src/styles/sassUtils/helpers.scss";
+  @import "./src/styles/sassUtils/query.scss";
+  `,
+  },
 }
 
 module.exports = nextConfig
